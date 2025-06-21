c4eb0ba4fc414e0386e08bc6375787d418d08e29
Deeksha Shetty <deekshashetty736@gmail.com>
2025-06-21 22:17:42 +0530

    added some projects

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero() {
             <div className="home-hero__content">
                 <h1 className="heading-primary">Hello, I am Deeksha</h1>
                 <div className="home-hero__info">
-                    <p className="text-primary">An experienced MERN Developer focussed on building Websites and Web Applications that leads to the success of the overall product</p>
+                    <p className="text-primary">An experienced Full Stack Developer focused on building Websites and Web Applications that leads to the success of the overall product</p>
                 </div>
                 <div className="home-hero__cta">
                     <a href="#projects" className="btn btn--bg">Projects</a>
--- frontend/src/data/projects.js
+++ frontend/src/data/projects.js
@@ -1,27 +1,47 @@
 const projects = [
-    // {
-    //     name: "Notes App 📝",
-    //     description: `A Full Stack App to store notes. User Signup and Login with JWT Authorization. User can add, delete notes. Used Context API for state management using React, NodeJS and Express for the backend, and MongoDB for the
-    //     database. `,
-    //     github: "https://github.com/shreyassp17/notes-app-frontend",
-    //     link: "https://helpful-eclair-9bb27c.netlify.app"
-    // },
-    // {
-    //     name: "Youtube Clone ▶️",
-    //     description: `A Web App to play Youtube videos with genre filter and video search feature.
-    //     Used ReactJS, MUI CSS framework for frontend
-    //     Used RapidAPI to fetch video details.`,
-    //     github: "https://github.com/shreyassp17/youtube-clone",
-    //     link: "https://coruscating-kashata-477d3b.netlify.app/"
-    // },
+  // {
+  //     name: "Notes App 📝",
+  //     description: `A Full Stack App to store notes. User Signup and Login with JWT Authorization. User can add, delete notes. Used Context API for state management using React, NodeJS and Express for the backend, and MongoDB for the
+  //     database. `,
+  //     github: "https://github.com/shreyassp17/notes-app-frontend",
+  //     link: "https://helpful-eclair-9bb27c.netlify.app"
+  // },
+  // {
+  //     name: "Youtube Clone ▶️",
+  //     description: `A Web App to play Youtube videos with genre filter and video search feature.
+  //     Used ReactJS, MUI CSS framework for frontend
+  //     Used RapidAPI to fetch video details.`,
+  //     github: "https://github.com/shreyassp17/youtube-clone",
+  //     link: "https://coruscating-kashata-477d3b.netlify.app/"
+  // },
 
-    {
+  {
     name: "WorldWise Web Application 🌍",
     description: `A travel tracking web application that allows users to register and log cities they have visited or plan to visit. Features include client-side routing for seamless navigation, global state management using React Context API and useReducer, scoped styling with Module CSS, and interactive maps integrated with React Leaflet for panning and zooming. Backend services handle user authentication, email verification, password management, and CRUD operations using Node.js, Express.js, MongoDB, and third-party tools like Nodemailer and JWT.`,
-    github: "https://github.com/deeksha-code",
-    link: "https://journeylogbydeeksha.netlify.app/"
-}
+    github: "https://github.com/deeksha-code/Worldwise-MERN",
+    link: "https://journeylogbydeeksha.netlify.app/",
+  },
+  {
+    name:"🍕Fast React Pizza Co",
+    description:"A simple pizza ordering app where users can choose pizzas from a menu and place an order without logging in. They just need to enter their name, phone number, and address. The menu comes from an API, and users can add multiple pizzas to a cart. They can also mark their order as “priority” for a small extra charge. After placing the order, they get a unique ID to track it, and can even mark it as priority later.",
+    github: "https://github.com/deeksha-code/16-fast-react-pizza",
+    link:"https://deeksha-fast-react-pizza.netlify.app/"
 
-]
+  },
+  {
+    name:"🏡The Wild Oasis",
+    description:"A hotel management web application designed for hotel staff to manage bookings, cabins, and guests. Employees must log in to use the app, and new accounts can only be created from inside the system. Users can update their profile, upload an avatar, and manage cabin data — including adding, editing, or deleting cabins with photos. The app displays all bookings with guest and payment details, allows checking guests in or out, and lets users mark bookings as “priority” or unpaid. It includes a dashboard to track key hotel stats, recent bookings, sales charts, and guest check-ins for the day.",
+    github: "https://github.com/deeksha-code/17-the-wild-oasis",
+    link:"https://17-the-wild-oasis-pink.vercel.app/"
+
+  },
+  {
+    name:"🌲 Wild Oasis – Reservation App (User Side)",
+    description:"A modern web application built using Next.js for hotel guests to explore cabins and make reservations. It supports dynamic routing, Tailwind CSS for design, and optimized image handling. Users can view cabin details, book stays, and interact with a responsive UI powered by React Server Components and App Router. Features like loading spinners, custom error handling, and optimized data fetching (using caching, revalidation, and server actions) make the app fast and user-friendly.",
+    github: "https://github.com/deeksha-code/21-the-wild-oasis-website",
+    link:"https://21-the-wild-oasis-website-one.vercel.app/"
+
+  },
+];
 
 export default projects;
